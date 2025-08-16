d6d699c34cb685bd2459c50fc08a8ee34f433b83
Kotaro <kota.kawaka@gmail.com_>
2025-08-16 11:51:09 +0700

    lab11 finish

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -20,7 +20,7 @@ export default function MainLayout() {
           </main>
 
           {/* Footer ด้านล่าง */}
-          <Footer year="2026" fullName="chanadda" studentId="67062039" />
+          <Footer year="202" fullName="Kotaro Kawakami" studentId="670610679" />
         </div>
       </div>
     </div>
